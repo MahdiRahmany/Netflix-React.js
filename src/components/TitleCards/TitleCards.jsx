af1ef9e3c1791f806df7ce10bf3323039d6462ad
Mahdi Rahmani <mahdirahmani.main@gmail.com>
2024-09-03 18:40:57 +0330

    display movies data from TMDB Api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,20 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import cards_data from "../../assets/cards/Cards_data";
 import "./TitleCards.css";
 import { useEffect } from "react";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardRef = useRef();
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YWE1NWRjMjU0ZDAzNjg2MzIwYmZhYTJhNDZhODlmYyIsIm5iZiI6MTcyNTM3NDYzMy4zNTM2OTYsInN1YiI6IjY2ZDcxNzM0ZWViYzBjYjAxN2RmODNkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PMZhJ96cL7rNlg49h6Qh5hdR4WuFZp376YiUK0pDGt0",
+    },
+  };
 
   const handleWheel = (e) => {
     e.preventDefault();
@@ -13,17 +23,30 @@ const TitleCards = ({ title, category }) => {
 
   useEffect(() => {
     cardRef.current.addEventListener("wheel", handleWheel);
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardRef}>
-        {cards_data.map((card) => {
+        {apiData.map((card) => {
           return (
             <div className="card" key={card}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
